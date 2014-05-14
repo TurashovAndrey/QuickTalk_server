5b6960af84a732c0b49f1155f537bb4700d9f2b1
Andrey Turashov <turashov@mail.ru>
2014-05-14 20:02:05 +0300

    Implementing comment user funtionality

---
+++ handlers/user_comments_handler.py
@@ -0,0 +1,11 @@
+from base_handler import BaseHandler
+from helpers.helper_functions import *
+from models.user_comments_model import UserCommentsModel
+
+
+class UserCommentsHandler(BaseHandler):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
